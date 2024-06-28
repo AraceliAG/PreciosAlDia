95ab6aa3ce9e78c968c2f7910d3e8867c4fc9785
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-06-28 13:35:27 -0600

    Se agrega ventana para cables de uso rudo

--- BuscadorPrecio/CableRudo.Designer.cs
+++ BuscadorPrecio/CableRudo.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CableRudo";
+            btBuscarPrecio = new Button();
+            cbMarca = new ComboBox();
+            cbCalibre = new ComboBox();
+            lblMarca = new Label();
+            lblCalibre = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // btBuscarPrecio
+            // 
+            btBuscarPrecio.Location = new Point(120, 159);
+            btBuscarPrecio.Name = "btBuscarPrecio";
+            btBuscarPrecio.Size = new Size(212, 34);
+            btBuscarPrecio.TabIndex = 13;
+            btBuscarPrecio.Text = "Buscar";
+            btBuscarPrecio.UseVisualStyleBackColor = true;
+            // 
+            // cbMarca
+            // 
+            cbMarca.FormattingEnabled = true;
+            cbMarca.Items.AddRange(new object[] { "Todas", "CONDULAC", "INDIANA", "" });
+            cbMarca.Location = new Point(120, 106);
+            cbMarca.Name = "cbMarca";
+            cbMarca.Size = new Size(212, 33);
+            cbMarca.TabIndex = 12;
+            // 
+            // cbCalibre
+            // 
+            cbCalibre.FormattingEnabled = true;
+            cbCalibre.Items.AddRange(new object[] { "2x14", "3x16", "3x14", "3x12", "3x10", "3x8", "4x12", "4x10", "4x8" });
+            cbCalibre.Location = new Point(120, 51);
+            cbCalibre.Name = "cbCalibre";
+            cbCalibre.Size = new Size(212, 33);
+            cbCalibre.TabIndex = 10;
+            // 
+            // lblMarca
+            // 
+            lblMarca.AutoSize = true;
+            lblMarca.Location = new Point(30, 109);
+            lblMarca.Name = "lblMarca";
+            lblMarca.Size = new Size(60, 25);
+            lblMarca.TabIndex = 9;
+            lblMarca.Text = "Marca";
+            // 
+            // lblCalibre
+            // 
+            lblCalibre.AutoSize = true;
+            lblCalibre.Location = new Point(30, 47);
+            lblCalibre.Name = "lblCalibre";
+            lblCalibre.Size = new Size(66, 25);
+            lblCalibre.TabIndex = 7;
+            lblCalibre.Text = "Calibre";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(30, 260);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(703, 225);
+            dataGridView1.TabIndex = 14;
+            // 
+            // CableRudo
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 509);
+            Controls.Add(dataGridView1);
+            Controls.Add(btBuscarPrecio);
+            Controls.Add(cbMarca);
+            Controls.Add(cbCalibre);
+            Controls.Add(lblMarca);
+            Controls.Add(lblCalibre);
+            Name = "CableRudo";
+            Text = "CableRudo";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btBuscarPrecio;
+        private ComboBox cbMarca;
+        private ComboBox cbCalibre;
+        private Label lblMarca;
+        private Label lblCalibre;
+        private DataGridView dataGridView1;
     }
 }
