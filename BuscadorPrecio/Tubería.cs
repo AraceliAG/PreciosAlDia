d3608ca854f143e2f1c36764baa49f7ef0b2ee8d
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-07-22 12:56:34 -0600

    SE TRABAJA EN LAS TUBERÍAS APENAS SE VAN A IMPLEMENTAR LA LÓGICA

---
+++ BuscadorPrecio/Tubería.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscadorPrecio
+{
+    public partial class Tubería : Form
+    {
+        public Tubería()
+        {
+            InitializeComponent();
+        }
+
+        private void cbTipoITM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbTipoTubería.Text == "Tubo FoGa, P/Delgada")
+            {
+                cbMedida.Items.AddRange(new object[]
+                {
+                    "1/2",
+                    "3/4",
+                    "1",
+                    "1 1/4"
+                });
+            }
+        }
+    }
+}
